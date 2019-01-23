531c035683bde0879515b0ba3d59654fb0c347a8
Yangxuan <yangxuan.1996@gmail.com>
2019-01-24 00:17:37 +0800

    rocket

---
+++ ProjectBoost/Assets/Rocket.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();	
+	}
+
+    private void ProcessInput(){
+        if (Input.GetKey(KeyCode.Space)){ // can thrust while rotating
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A)){
+            print("rotate left");
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            print("rotate right");
+        }
+    }
+}
